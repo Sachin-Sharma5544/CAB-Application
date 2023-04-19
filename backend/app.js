3135016b493528b0c813196e2df868fb54d36d38
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-04-19 15:03:31 +0530

    Socket.io package installed on backend

--- backend/app.js
+++ backend/app.js
@@ -17,19 +17,21 @@ app.use((req, res, next) => {
     console.log(req.method, req.path);
     next();
 });
+
 app.use(express.json());
 
 app.use(cors());
-
-app.use("/customer", customerAuthRoute);
-app.use("/driver", driverAuthRoute);
 
 //This is used for testing booking model
 app.use("/rental-ride", rentalRideRoute);
 
 app.use("/car-ride", carRideRoute);
 app.use("/bike-ride", bikeRideRoute);
+
+//These are working routes added as per the feature
 app.use("/vehicle", vehicleRoute);
+app.use("/customer", customerAuthRoute);
+app.use("/driver", driverAuthRoute);
 
 mongoose.connect(process.env.MONGO_DB_URL).then(() => {
     console.log("Database connected successfully");
